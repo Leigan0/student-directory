99e4869cfadee6866e83a453965a0510eccfacc5
Leigh-ann <leighann.gant@gmail.com>
2017-12-13 18:50:04 +0000

    Code refactored to remove magic number

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
+
+
 def input_students
-  puts "Please enter students details".center(65)
-  puts "To finish, just hit return twice".center(65)
+  width = 65
+  puts "Please enter students details".center(width)
+  puts "To finish, just hit return twice".center(width)
   #create an empty array
   students = []
   #get first name
   name = gets.chomp
   #while the name is not empty - repeat this code
   while !name.empty? do
-    puts "Please enter student age".center(65)
+    puts "Please enter student age".center(width)
     age = gets.chomp
-    puts "Please enter student nationality".center(65)
+    puts "Please enter student nationality".center(width)
     nationality = gets.chomp
     students << {name: name, cohort: :november, age: age, nationality: nationality}
-    puts "Now we have #{students.count} students".center(65)
+    puts "Now we have #{students.count} students".center(width)
     name = gets.chomp
   end
   #return array of students
@@ -20,20 +23,23 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(65)
-  puts "-------------".center(65)
+  width = 65
+  puts "The students of Villains Academy".center(width)
+  puts "-------------".center(width)
 end
 
 def print(students)
+  width = 65
   counter = 0
   while counter < students.count
-        puts " #{counter+1}. #{students[counter][:name]}, #{students[counter][:age]}, #{students[counter][:nationality]} (#{students[counter][:cohort]} cohort)".center(65)
+        puts " #{counter+1}. #{students[counter][:name]}, #{students[counter][:age]}, #{students[counter][:nationality]} (#{students[counter][:cohort]} cohort)".center(width)
       counter += 1
     end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(65)
+  width = 65
+  puts "Overall, we have #{students.count} great students".center(width)
 end
 #nothing happens until we call the methods
 students = input_students
