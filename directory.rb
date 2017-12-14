4fb995a0f40eec87cfde0280b7fd4c592e80a2d2
Leigh-ann <leighann.gant@gmail.com>
2017-12-14 17:46:09 +0000

    Input student method refactored

--- directory.rb
+++ directory.rb
@@ -1,11 +1,12 @@
 require 'date'
 
 @students = [] #an empty array accessible to all methods
+@width = 65
 
 def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
+  puts "1. Input the students".center(@width)
+  puts "2. Show the students".center(@width)
+  puts "9. Exit".center(@width)
 end
 
 def show_students
@@ -19,11 +20,11 @@ def process(selection)
     when "1"
     input_students
     when "2"
-      print_students_list
+      show_students
     when "9"
       exit
     else
-      puts "I don't know what you meant, try again"
+      puts "I don't know what you meant, try again".center(@width)
   end
 end
 
@@ -35,48 +36,53 @@ def interactive_menu
 end
 
 def input_students
-  width = 65
-  puts "Please enter each students details, starting with student name".center(width)
-  puts "To finish, just hit return twice".center(width)
-
+  input_instruction_text
   name = gets.delete("\n")
   while !name.empty? do
-    puts "please enter student cohort, if blank cohort will default to november"
-    cohort = gets.delete("\n")
-     until Date::MONTHNAMES.include?(cohort.capitalize) || cohort == ''
-       puts "Please enter a valid cohort".center(width)
-       cohort = gets.delete("\n")
-     end
-    if cohort == ''then cohort = :november end
+    cohort = cohort_entry
     @students << {name: name, cohort: cohort.downcase.to_sym}
-    @students.count > 1 ? puts("Now we have #{@students.count} students".center(width)) : puts("Now we have 1 student".center(width))
+    @students.count > 1 ? puts("Now we have #{@students.count} students".center(@width)) : puts("Now we have 1 student".center(@width))
     name = gets.delete("\n")
   end
 end
 
+def input_instruction_text
+  puts "Please enter each students details, starting with student name".center(@width)
+  puts "To finish, just hit return twice".center(@width)
+end
+
+def cohort_entry
+  puts "please enter student cohort, if blank cohort will default to november".center(@width)
+  cohort = gets.delete("\n")
+  until Date::MONTHNAMES.include?(cohort.capitalize) || cohort == ''
+    puts "Please enter a valid cohort".center(@width)
+    cohort = gets.delete("\n")
+  end
+  if cohort == ''then cohort = :november end
+  return cohort
+end
+
 def print_header
   width = 65
-  puts "The students of Villains Academy".center(width)
-  puts "-------------".center(width)
+  puts "The students of Villains Academy".center(@width)
+  puts "-------------".center(@width)
 end
 
 def print_students_list
-  width = 65
   return if  @students.count < 1
   cohorts = @students.map {|student| student[:cohort]}.uniq
   cohorts.each do |cohort|
     @students.each do |student|
       if student[:cohort] == cohort
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+        puts "#{student[:name]} (#{student[:cohort]} cohort)".center(@width)
       end
     end
   end
 end
 
 def print_footer
-  return if  @students.count < 1
-  width = 65
-  @students.count > 1 ? puts("Overall, we have #{@students.count} great students".center(width)) : puts("Overall, we have 1 great student")
+  return if  @students.count == 0
+  @students.count > 1 ? puts("Overall, we have #{@students.count} great students".center(@width)) : puts("Overall, we have 1 great student".center(@width))
 end
 
 interactive_menu
